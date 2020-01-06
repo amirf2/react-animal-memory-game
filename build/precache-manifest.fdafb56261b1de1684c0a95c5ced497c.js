557f6d1f0aeccfa4e5050d92154fa5534de1c7f4
amirf2 <40579045+amirf2@users.noreply.github.com>
2020-01-06 16:29:32 +0200

    update

---
+++ build/precache-manifest.fdafb56261b1de1684c0a95c5ced497c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7eed5758df5e95180fd81f6aa5d53a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8e101fb28f0f5b3a294",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "9ea58dd739d24afe35df",
+    "url": "/static/js/2.331925e2.chunk.js"
+  },
+  {
+    "revision": "c8e101fb28f0f5b3a294",
+    "url": "/static/js/main.ee55acf0.chunk.js"
+  },
+  {
+    "revision": "911b4e50453077f751e9",
+    "url": "/static/js/runtime-main.77d2b3a3.js"
+  }
+]);
